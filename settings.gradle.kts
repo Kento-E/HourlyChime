6a5661971afe2a0b405684049a1f17f46729406b
shika-e <Kento-E@users.noreply.github.com>
2026-05-11 00:16:33 +0900

    ✨ feat(pluginManagement): プラグインの解決戦略を追加

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,17 @@
 pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.android.application" && requested.version != null) {
+                useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+            if (requested.id.id == "com.google.gms.google-services" && requested.version != null) {
+                useModule("com.google.gms:google-services:${requested.version}")
+            }
+            if (requested.id.id == "com.google.firebase.appdistribution" && requested.version != null) {
+                useModule("com.google.firebase:firebase-appdistribution-gradle:${requested.version}")
+            }
+        }
+    }
     repositories {
         google {
             content {
@@ -14,6 +27,10 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        mavenLocal()
+        maven {
+            url = uri("$rootDir/local-maven")
+        }
         google()
         mavenCentral()
     }
